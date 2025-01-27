d78701b92a83369d26ad40e6e87fdfb52492d964
Anas <anass.spk@gmail.com>
2025-01-27 22:09:21 +0100

    fine rn. all working, fixed styling for student list in class details

--- SchoolManagement_Mvc/obj/Debug/net8.0/SchoolManagement_Mvc.AssemblyInfo.cs
+++ SchoolManagement_Mvc/obj/Debug/net8.0/SchoolManagement_Mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagement_Mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d07126f8fa9f34d6740be5d0bf836d905827eab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a37742a4c541a335e39f1e8022195ea377b7c51")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagement_Mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagement_Mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
